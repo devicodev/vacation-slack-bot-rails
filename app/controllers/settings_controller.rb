912265231d897c011580c4583838016d0b94206a
Ice5050 <ice50505050@hotmail.com>
2016-03-04 14:20:42 +0700

    Add Setup phase

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,16 +6,20 @@ class SettingsController < ApplicationController
   def update
     load_setting
     @setting.update(setting_params)
-    render 'edit'
+    if @setting.previous_changes.key?('google_client_id') || @setting.previous_changes.key?('google_client_secret')
+      redirect_to new_session_path
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def load_setting
-    @setting ||= Setting.first
+    @setting ||= Setting.first_or_create
   end
 
   def setting_params
-    params.require(:setting).permit(:slack_api_token, :google_calendar_id)
+    params.require(:setting).permit(:google_client_id, :google_client_secret, :slack_api_token, :google_calendar_id)
   end
 end
